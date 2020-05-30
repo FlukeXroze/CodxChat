ebbe9d0f3e53e947a0e752418bcc6347e8ded08d
FlukeXroze <49242853+FlukeXroze@users.noreply.github.com>
2020-05-30 12:22:16 +0700

    11

---
+++ app/src/main/java/com/trap9/codxchat/activities/YourProfileActivity.kt
@@ -0,0 +1,60 @@
+package com.trap9.codxchat.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import com.trap9.codxchat.R
+import kotlinx.android.synthetic.main.activity_your_profile.*
+
+class YourProfileActivity : AppCompatActivity() {
+
+    var mAuth:FirebaseAuth? = null
+    var mDatabase:FirebaseDatabase? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_your_profile)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance()
+
+        var userId = mAuth!!.currentUser!!.uid
+
+        var userRef = mDatabase!!.reference.child("Users").child(userId)
+
+        userRef.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+                finish()
+            }
+
+            override fun onDataChange(dataSnapshot : DataSnapshot) {
+                var display_name = dataSnapshot.child("display_name").value.toString()
+                var status = dataSnapshot.child("status").value.toString()
+                var image = dataSnapshot.child("image").value.toString()
+
+                tv_display_name_profile.text = display_name
+                tv_status_profile.text = status
+
+                if (image != null)
+                    Picasso.get().load(image).placeholder(R.drawable.ic_person).into(iv_image_profile)
+            }
+        })
+        btn_change_username.setOnClickListener{
+            var intent = Intent(this,UpdateDisplaynameActivity::class.java)
+            intent.putExtra("Display_name",tv_display_name_profile.text.toString())
+            startActivity(intent)
+        }
+
+        btn_update_status.setOnClickListener{
+            var intent = Intent(this,UpdateStatusActivity::class.java)
+            intent.putExtra("Display_name",tv_status_profile.text.toString())
+            startActivity(intent)}
+    }
+
+}
